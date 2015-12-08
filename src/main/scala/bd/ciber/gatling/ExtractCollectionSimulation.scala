5ab692dc38e4849e77fe6b5da20ac69443d1e638
Gregory Jansen <gregory.jansen@gmail.com>
2015-12-08 16:29:54 -0500

    foo

--- src/main/scala/bd/ciber/gatling/ExtractCollectionSimulation.scala
+++ src/main/scala/bd/ciber/gatling/ExtractCollectionSimulation.scala
@@ -72,8 +72,7 @@ class ExtractCollectionSimulation extends Simulation {
       exec(http("pollUrl")
         .get(dtsUrl + "/api/extractions/${id}/status?key="+commkey)
         .headers(headers_accept_json)
-        .check(bodyString.exists.saveAs("body")
-            )
+        .check(bodyString.exists.saveAs("body"))
       ).exec(session => {
           println(session("body").as[String])
           session
