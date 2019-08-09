b481ad7a37e060e70fb4f72d4901f3fb6178655a
Jarlath Holleran <jarlathh@inrupt.com>
2019-08-09 16:16:09 +0100

    reverted the ldp server name

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -e SIM_CLASS="ldp.StressTestIngest" -e LDP_URL="http://enterpriseserver_trellis_1:8080" \
+docker run -e SIM_CLASS="ldp.StressTestIngest" -e LDP_URL="http://trellis:8080" \
 -e ELASTICSEARCH_URL="http://elasticsearch:9200/" \
 -e LOCAL_PATH_PREFIX="/srv/ciber" \
 -v nfs-ciber:/srv/ciber \
